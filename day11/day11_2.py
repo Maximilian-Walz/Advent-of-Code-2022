1f24e62fc1f6459b9f38ab481e7e5189cd1be376
Maximilian Walz <github@maximilian-walz.com>
2022-12-13 11:15:58 +0100

    ADD: Solutions for day 11.2 and correct input for day 11

---
+++ day11/day11_2.py
@@ -0,0 +1,44 @@
+import re
+
+with open("input.txt") as file:
+    # Parse the input
+    monkeys_input = file.read().split("\n\n")
+    monkeys = []
+    for monkey_input in monkeys_input:
+        monkey = {"item_count": 0}
+        monkey["items"] = [int(item) for item in re.search(r'Starting items: (.*)', monkey_input).group(1).split(", ")]
+        monkey["operation"] = re.search(r'Operation: new = old (.*)', monkey_input).group(1).split(" ")
+        monkey["test_value"] = int(re.search(r'Test: divisible by (\d*)', monkey_input).group(1))
+        monkey["true_target"] = int(re.search(r'If true: throw to monkey (\d*)', monkey_input).group(1))
+        monkey["false_target"] = int(re.search(r'If false: throw to monkey (\d*)', monkey_input).group(1))
+        monkeys.append(monkey)
+
+    bigModulo = 1
+    for monkey in monkeys:
+        bigModulo *= monkey["test_value"]
+
+    # 20 Rounds
+    for i in range(10000):
+        for monkey in monkeys:
+            while len(monkey["items"]) > 0:
+                # Monkey inspects item
+                item = monkey["items"].pop(0)
+                monkey["item_count"] += 1
+                value = item if monkey["operation"][1] == "old" else int(monkey["operation"][1])
+                if monkey["operation"][0] == "+":
+                    item += value
+                elif monkey["operation"][0] == "*":
+                    item *= value
+
+                # Monkey tests item
+                target = monkey["true_target"] if item % monkey["test_value"] == 0 else monkey["false_target"]
+                item %= bigModulo
+                
+                # Monkey throws item
+                monkeys[target]["items"].append(item)
+
+
+    for monkey in monkeys:
+        print(monkey["item_count"])
+    print((lambda x: x[0] * x[1])(sorted([monkey["item_count"] for monkey in monkeys], reverse=True)[:2]))
+    
